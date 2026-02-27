0c298f59993538264e0e3264cd12f539aaf446fc
Bindhugs <b6243451@gmail.com>
2026-02-27 19:08:04 +0530

    Array

---
+++ Array.py
@@ -0,0 +1,9 @@
+#Find the largest element in an array
+a = list(map(int,input("Enter the elements of the array: ").split()))
+#takes user input and converts it into a list of integers
+
+largest = a[0]
+for i in range(1,len(a)):
+    if a[i] > largest: #checks if the current element is largest than the larger element
+        largest = a[i] 
+print("The largest element in the array is: ",largest) #prints the largest element in the array
