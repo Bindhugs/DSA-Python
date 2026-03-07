67213774154ec6f78be736ffb9c28a7b7d842872
Bindhugs <b6243451@gmail.com>
2026-03-07 21:27:30 +0530

    Bubble_sort.py

---
+++ Bubble_Sort.py
@@ -0,0 +1,10 @@
+a = [75, 95, 62, 15, 54, 32]
+n = len(a)
+
+for i in range(n):
+    for j in range(0,n-i-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+print("Sorted array is:")
+for i in range(n):
+    print(a[i])
