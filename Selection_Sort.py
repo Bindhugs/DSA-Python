d2bd692739534f48e0cc0ca25d3084ae91ffbb6f
Bindhugs <b6243451@gmail.com>
2026-03-08 20:10:40 +0530

    Selection_Sort.py

---
+++ Selection_Sort.py
@@ -0,0 +1,19 @@
+arr = []
+
+n = int(input("Enter number of elements: "))
+
+for i in range(n):
+    val = int(input("Enter element: "))
+    arr.append(val)
+
+# Selection Sort
+for i in range(n):
+    min_index = i
+
+    for j in range(i+1, n):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print("Sorted array:", arr)
