bbd8be649d7d91571241213aa7d927700f357bb7
BindhuGS <b6243451@gmail.com>
2026-03-29 20:55:31 +0530

    numpy.py

---
+++ NumPy/Examples2.py
@@ -0,0 +1,7 @@
+#prints multiplication tables
+
+import numpy as np
+
+array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
+array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+print(array1*array2)
