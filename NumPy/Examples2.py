e29b9a844aa541631a8c7af9c27dd6aa69fb97ec
BindhuGS <b6243451@gmail.com>
2026-03-29 21:14:35 +0530

    numpy.py

--- NumPy/Examples2.py
+++ NumPy/Examples2.py
@@ -8,7 +8,7 @@ print(array1*array2)
 
 
 
-array = np.array([[1,2,3,4,5,6,7,8,9,10]])
+array = np.array([1,2,3,4,5,6,7,8,9,10])
 print(np.sum(array)) #sum of all elements
 print(np.mean(array)) #mean 
 print(np.max(array)) #maximum
